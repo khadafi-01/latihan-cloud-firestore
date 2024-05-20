45fd5525a3cd75fb41b88b101491eb16283f4bd2
Khadafi <khadafiahmad01@gmail.com>
2024-05-21 06:58:14 +0700

    Buat collection pada root-level dengan nama dokter.

--- firestore.js
+++ firestore.js
@@ -3,3 +3,10 @@ const { Firestore } = require("@google-cloud/firestore")
 
 // Create a Firestore client and store it in the db variable
 const db = new Firestore();
+
+// Create a collection at the root-level with the name “doctor”
+async function store_data() {
+    // Membuat Collection root-level
+    const doctorsCollections = db.collection('dokter');
+    console.log("Collections 'dokter' berhasil dibuat.");
+}
