85346f1042dc7de1b6a7afe1a09a288b1038cb9b
Khadafi <khadafiahmad01@gmail.com>
2024-05-21 08:14:27 +0700

    mengubah versi firestore dan kode firestore.js

--- firestore.js
+++ firestore.js
@@ -6,37 +6,37 @@ const db = new Firestore();
 
 // Create a collection at the root-level with the name “doctor”
 async function store_data() {
+
     // Membuat Collection root-level
-    const doctorsCollections = db.collection('dokter');
+    const doctorsCollections = db.collection('dokter-db');
     console.log("Collections 'dokter' berhasil dibuat.");
 
-    // Create Doctor Eros documents
+    // Membuat dokumen: Dokter Eros
     const erosDoc = await doctorsCollections.doc("Dokter Eros");
-    console.log("Dokumen atas nama Dokter Eros berhasil dibuat")
+    console.log("Dokumen atas nama Dokter Eros berhasil dibuat.");
+
+    // Menambahkan data pribadi Dokter Eros
+    const profileEros = {
+        nama: "dr. Eros",
+        keahlian: "Dokter Kulit",
+        almamater: "Universitas A"
+    }
+    await erosDoc.set(profileEros);
+    console.log("Data berhasil ditambahkan ke dokumen Eros");
+
+    // Membuat subcollection: Konsultasi
+    const erosSubcollections = erosDoc.collection("Konsultasi");
+    console.log("Subcollection Konsultasi berhasil dibuat.");
+
+    // Menambahkan data ke 
+    const historyConsultations = {
+        nama_pasien: {
+            depan: "Antony",
+            belakang: "Gunawan",
+        },
+        waktu_konsultasi: Date.now().toString()
+    }
+    await erosSubcollections.doc("Antony").set(historyConsultations);
+    console.log("Data berhasil ditambahkan.");
 }
-
-// Adding Doctor Eros' personal data
-const profileEros = {
-    nama: "dr. Eros",
-    keahlian: "Dokter Kulit",
-    almamater: " Univeristas A"
-}
-await erosDoc.set(profileEros);
-console.log("Data berhasil ditamabahkan ke dokumen Eros");
-
-// Create a subcollection: Consultation 
-const erosSucollections = erosDoc.collection("Konsultasi");
-console.log("Subcollection Konsultasi berhasil dibuat");
-
-// Adding data to patient documents
-const historyConsultations = {
-    nama_pasien: {
-        depan: "Anthony",
-        belakang: "Gunawan",
-    },
-    waktu_konsultasi: Date.now().toString()
-}
-await erosSubcollections.doc("Antony").set(historyConsultations);
-console.log("Data berhasil ditambahkan.");
-
 store_data().catch(console.error)
